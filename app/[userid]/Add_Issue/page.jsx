f4f26fb1979f63b597cc2f0a3b5a17c781159883
ecappz3420 <info@ecappz.in>
2025-04-11 13:14:36 +0530

    Access restriction

--- app/[userid]/Add_Issue/page.jsx
+++ app/[userid]/Add_Issue/page.jsx
@@ -1,24 +1,23 @@
-import React from 'react'
-import { ChakraProvider } from '@chakra-ui/react'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import Form from './Form'
-import Header from './Header'
-import { fetchVehicles, refreshAccessToken } from '@/app/utils/zohoAuth'
-const FormPage = async ({params}) => {
+import React from "react";
+import { ChakraProvider } from "@chakra-ui/react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Form from "./Form";
+import Header from "./Header";
+import { fetchVehicles, refreshAccessToken } from "@/app/utils/zohoAuth";
+const FormPage = async ({ params }) => {
+  const access_token = await refreshAccessToken();
+  const records = await fetchVehicles(access_token);
+  return (
+    <ChakraProvider>
+      <div className="container-fluid p-2 vh-100 d-flex flex-column">
+        <Header />
+        <div className="flex-grow-1 d-flex flex-column justify-content-center align-items-center">
+          {/* <Form records={records} userid={params.userid}/> */}
+          <div className="text-center">Page not found!</div>
+        </div>
+      </div>
+    </ChakraProvider>
+  );
+};
 
-    const access_token = await refreshAccessToken();
-    const records = await fetchVehicles(access_token);
-    return (
-        <ChakraProvider>
-            <div className="container-fluid p-2 vh-100 d-flex flex-column">
-                <Header />
-                <div style={{ flex: '1' }} className='overflow-y-auto custom-scroll'>
-                    <Form records={records} userid={params.userid}/>
-                </div>
-            </div>
-
-        </ChakraProvider>
-    )
-}
-
-export default FormPage
+export default FormPage;
